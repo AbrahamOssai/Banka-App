42e57ec7fdb2cc47bd2c099f8029903e34f67a2e
Ifeanyichukwu Abraham Ossai <abraham.ossai@gmail.com>
2019-04-11 03:13:39 +0100

    chore(restructure-cleanup):restructure and cleanup repo

---
+++ api/test/app-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import app from '../app';
+
+describe('app', function() {
+    it('should exist', function() {
+        expect(app).to.not.be.undefined;
+    });
+});
