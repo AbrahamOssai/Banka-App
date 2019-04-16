dccfa7ae9c2af5895fc6145250a51f4dd6dfee03
Ifeanyichukwu Abraham Ossai <abraham.ossai@gmail.com>
2019-04-16 19:29:39 +0100

    chore(heroku):add config for heroku A configuration file for heroku was added

--- api/index.js
+++ api/index.js
@@ -2,17 +2,19 @@ import express, { Router } from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import v1routes from './version/version1';
-// body parser to read the data
+
 
 // instantiate expressjs
 const app = express();
-const PORT = process.env.PORT || 5900;
+
+const PORT = process.env.PORT || 5000;
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', v1routes({ Router }));
 app.get('/', (req, res) => res.send({ message: 'Made it to the Root. Welcome...' }).status(200));
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+app.listen(PORT);
 
 export default app;
