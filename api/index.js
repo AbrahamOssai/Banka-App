c949a393c294da0b26e5dc5b4edf56f8718ace28
Ifeanyichukwu Abraham Ossai <abraham.ossai@gmail.com>
2019-04-19 16:59:44 +0100

    chore(starts):[Starts #165470587]

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@ app.use('/api/v1', v1routes({ Router }));
 app.get('/', (req, res) => res.send({ message: 'Made it to the Root. Welcome...' }).status(200));
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
-  open('C:/Users/IFEANYICHUKWU/repos/Banka-App/index.html');
 });
 
 export default app;
